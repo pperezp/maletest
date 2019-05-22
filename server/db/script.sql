5df54026ce53334e52883e9bdcb1639ff56b83d2
Patricio Pérez Pinto <patodeath@gmail.com>
2019-05-21 21:15:08 -0400

    Se hacen cambios en la base de datos, se añade tabla llamada session. Se añaden inserts y select de porcentaje

--- server/db/script.sql
+++ server/db/script.sql
@@ -15,15 +15,28 @@ CREATE TABLE question(
     PRIMARY KEY(id)
 );
 
+CREATE TABLE session(
+    uuid VARCHAR(36),
+    date datetime,
+    PRIMARY KEY(uuid)
+);
+
 CREATE TABLE answer(
     id INT AUTO_INCREMENT,
     userId INT,
     questionId INT,
     answer BOOLEAN,
+    sessionId VARCHAR(36),
     PRIMARY KEY(id),
     FOREIGN KEY(userId) REFERENCES user(id),
-    FOREIGN KEY(questionId) REFERENCES question(id)
+    FOREIGN KEY(questionId) REFERENCES question(id),
+    FOREIGN KEY(sessionId) REFERENCES session(uuid)
 );
+
+INSERT INTO user VALUES
+(NULL, 'Patricio Pérez Pinto', 'pperezp@gmail.com');
+
+INSERT INTO session VALUES('baafaa2d-7c2d-11e9-891d-c7d8e6f51aa0', NOW());
 
 INSERT INTO question VALUES
 (NULL, '¿Eres experto en Futbol?'),
@@ -61,6 +74,66 @@ INSERT INTO question VALUES
 (NULL, '¿Te has depilado?'),
 (NULL, '¿Has matado a algún animal?');
 
+INSERT INTO answer VALUES
+(NULL, 1, 1,TRUE, 'baafaa2d-7c2d-11e9-891d-c7d8e6f51aa0'),
+(NULL, 1, 2,TRUE, 'baafaa2d-7c2d-11e9-891d-c7d8e6f51aa0'),
+(NULL, 1, 3,FALSE, 'baafaa2d-7c2d-11e9-891d-c7d8e6f51aa0'),
+(NULL, 1, 4,FALSE, 'baafaa2d-7c2d-11e9-891d-c7d8e6f51aa0'),
+(NULL, 1, 5,TRUE, 'baafaa2d-7c2d-11e9-891d-c7d8e6f51aa0'),
+(NULL, 1, 6,TRUE, 'baafaa2d-7c2d-11e9-891d-c7d8e6f51aa0'),
+(NULL, 1, 7,TRUE, 'baafaa2d-7c2d-11e9-891d-c7d8e6f51aa0'),
+(NULL, 1, 8,FALSE, 'baafaa2d-7c2d-11e9-891d-c7d8e6f51aa0'),
+(NULL, 1, 9,TRUE, 'baafaa2d-7c2d-11e9-891d-c7d8e6f51aa0'),
+(NULL, 1, 10,TRUE, 'baafaa2d-7c2d-11e9-891d-c7d8e6f51aa0'),
+(NULL, 1, 11,TRUE, 'baafaa2d-7c2d-11e9-891d-c7d8e6f51aa0'),
+(NULL, 1, 12,TRUE, 'baafaa2d-7c2d-11e9-891d-c7d8e6f51aa0'),
+(NULL, 1, 13,FALSE, 'baafaa2d-7c2d-11e9-891d-c7d8e6f51aa0'),
+(NULL, 1, 14,FALSE, 'baafaa2d-7c2d-11e9-891d-c7d8e6f51aa0'),
+(NULL, 1, 15,TRUE, 'baafaa2d-7c2d-11e9-891d-c7d8e6f51aa0'),
+(NULL, 1, 16,FALSE, 'baafaa2d-7c2d-11e9-891d-c7d8e6f51aa0'),
+(NULL, 1, 17,TRUE, 'baafaa2d-7c2d-11e9-891d-c7d8e6f51aa0'),
+(NULL, 1, 18,TRUE, 'baafaa2d-7c2d-11e9-891d-c7d8e6f51aa0'),
+(NULL, 1, 19,TRUE, 'baafaa2d-7c2d-11e9-891d-c7d8e6f51aa0'),
+(NULL, 1, 20,FALSE, 'baafaa2d-7c2d-11e9-891d-c7d8e6f51aa0'),
+(NULL, 1, 21,TRUE, 'baafaa2d-7c2d-11e9-891d-c7d8e6f51aa0'),
+(NULL, 1, 22,TRUE, 'baafaa2d-7c2d-11e9-891d-c7d8e6f51aa0'),
+(NULL, 1, 23,FALSE, 'baafaa2d-7c2d-11e9-891d-c7d8e6f51aa0'),
+(NULL, 1, 24,TRUE, 'baafaa2d-7c2d-11e9-891d-c7d8e6f51aa0'),
+(NULL, 1, 25,TRUE, 'baafaa2d-7c2d-11e9-891d-c7d8e6f51aa0'),
+(NULL, 1, 26,TRUE, 'baafaa2d-7c2d-11e9-891d-c7d8e6f51aa0'),
+(NULL, 1, 27,TRUE, 'baafaa2d-7c2d-11e9-891d-c7d8e6f51aa0'),
+(NULL, 1, 28,FALSE, 'baafaa2d-7c2d-11e9-891d-c7d8e6f51aa0'),
+(NULL, 1, 29,TRUE, 'baafaa2d-7c2d-11e9-891d-c7d8e6f51aa0'),
+(NULL, 1, 30,FALSE, 'baafaa2d-7c2d-11e9-891d-c7d8e6f51aa0'),
+(NULL, 1, 31,TRUE, 'baafaa2d-7c2d-11e9-891d-c7d8e6f51aa0'),
+(NULL, 1, 32,TRUE, 'baafaa2d-7c2d-11e9-891d-c7d8e6f51aa0'),
+(NULL, 1, 33,FALSE, 'baafaa2d-7c2d-11e9-891d-c7d8e6f51aa0'),
+(NULL, 1, 34,FALSE, 'baafaa2d-7c2d-11e9-891d-c7d8e6f51aa0');
+
 SELECT * FROM user;
 SELECT * FROM question;
+SELECT * FROM session;
 SELECT * FROM answer;
+
+DROP TABLE answer;
+DROP TABLE session;
+DROP TABLE question;
+DROP TABLE user;
+
+-- Porcentaje de male
+SELECT 
+    (
+        (
+            SELECT 
+                COUNT(0) 
+            FROM 
+                answer 
+            WHERE 
+                answer IS TRUE AND 
+                userId = 1 AND
+                sessionId = 'baafaa2d-7c2d-11e9-891d-c7d8e6f51aa0'
+        ) /
+        (
+            SELECT COUNT(0) FROM answer
+        ) * 100
+    ) 'Porcentaje';
